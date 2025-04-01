99074d22deddbda8ee6070660aa14a2e4c9ac230
Youssuf Shakweh <youssuf@dhcppc9.com>
2025-04-01 15:53:23 +0300

    🔧 Add core configuration and environment setup

---
+++ src/core/config.py
@@ -0,0 +1,39 @@
+import secrets
+from typing import Literal
+
+from pydantic import computed_field
+from pydantic_core import MultiHostUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file="../../.env", env_ignore_empty=True, extra="ignore"
+    )
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 2
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_DB: str = ""
+
+    @computed_field
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> str:
+        return str(
+            MultiHostUrl.build(
+                scheme="postgresql+psycopg",
+                username=self.POSTGRES_USER,
+                password=self.POSTGRES_PASSWORD,
+                host=self.POSTGRES_SERVER,
+                port=self.POSTGRES_PORT,
+                path=self.POSTGRES_DB,
+            )
+        )
+
+
+settings = Settings()
